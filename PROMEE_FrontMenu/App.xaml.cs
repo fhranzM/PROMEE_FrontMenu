9a2f10ce628fd841133d2af123104e72fde85970
user <user@DESKTOP-RHPBDJP>
2023-10-18 13:47:03 +0800

    Add project files.

---
+++ PROMEE_FrontMenu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PROMEE_FrontMenu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PROMEE_FrontMenu/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PROMEE_FrontMenu
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
